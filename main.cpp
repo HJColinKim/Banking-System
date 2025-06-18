fb54dc3af41773817281cc8b7183f417c66dcc5c
Hyungjae (Colin) Kim <hjcolinkim@gmail.com>
2025-06-19 01:26:48 +1200

    Basic start to banking system, with Account class. Will have to add a database system for bank-owners to manage accounts.

--- main.cpp
+++ main.cpp
@@ -5,3 +5,29 @@
 #include<vector>
 #include "header.h"
 
+
+class Account{ 
+
+    double balance{0}; 
+    std::string accountName{"ACCOUNT-HOLDER"}; 
+
+    public: 
+    Account(){}; //Default constructor 
+
+    Account(std::string_view name) //Constructor with name 
+    : accountName(name), balance(0) {}; 
+
+    //Arguments for getting 
+    const std::string_view getName(){
+        return accountName; 
+    }
+
+    double getBalance(){
+        return balance; 
+    }
+
+    //Arguments for transfering 
+    void transferMoney(double amount, Account account){}
+
+    
+}; 
