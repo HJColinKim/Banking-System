7199cda6b06864ae37ab3867510895ef5aed60ca
Hyungjae (Colin) Kim <hjcolinkim@gmail.com>
2025-06-18 23:57:53 +1200

    Setting up relevant files for proecjt

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream> 
+#include<fstream>
+#include<string>
+#include<string_view>
+#include<vector>
+#include "header.h"
+
---
+++ source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+namespace accountInteractions {
+
+}
+
+struct  {
+    
+};
+
+
+
+int main(){
+    return 0; 
+}
