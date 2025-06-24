ece29422a92ade722eddb44fac90e2966e4efbf9
Hyungjae (Colin) Kim <hjcolinkim@gmail.com>
2025-06-25 01:28:42 +1200

    Added adjustments, putting classes in header files with concrete method declarations in the main cpp file.

--- header.h
+++ header.h
@@ -1,6 +1,24 @@
 #pragma once //header file guard 
+#include <string>
 
-int add(double x, double y){
-    return x + y; 
-}
+class Account{ 
 
+    double balance; 
+    std::string accountName; 
+
+    public: 
+    Account(){}; //Default constructor 
+
+    Account(std::string_view name) //Constructor with name 
+    : accountName(name), balance(0) {}; 
+
+    //Arguments for getting 
+    const std::string_view getName() const; 
+
+    double getBalance(); 
+    
+    //Arguments for transfering 
+    void transferMoney(double amount, Account& account); 
+
+    
+}; 
--- main.cpp
+++ main.cpp
@@ -1,33 +1,22 @@
-#include <iostream> 
-#include<fstream>
-#include<string>
-#include<string_view>
-#include<vector>
+#include <iostream>
 #include "header.h"
 
+const std::string_view Account::getName() const{
+    return accountName; 
+};
 
-class Account{ 
+double Account::getBalance(){
+    return balance; 
+}
 
-    double balance{0}; 
-    std::string accountName{"ACCOUNT-HOLDER"}; 
-
-    public: 
-    Account(){}; //Default constructor 
-
-    Account(std::string_view name) //Constructor with name 
-    : accountName(name), balance(0) {}; 
-
-    //Arguments for getting 
-    const std::string_view getName(){
-        return accountName; 
-    }
-
-    double getBalance(){
-        return balance; 
-    }
-
-    //Arguments for transfering 
-    void transferMoney(double amount, Account account){}
-
+void Account::transferMoney(double amount, Account& account){
     
-}; 
+} 
+
+
+int main(){
+    Account account1 = Account("Colin Kim"); 
+    std::cout << account1.getName() << "\n"; 
+    
+    return 0; 
+}
